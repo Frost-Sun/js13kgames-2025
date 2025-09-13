7a06838db028f90333338c9039d44272d8cc60df
Tero Jäntti <tkjantti@gmail.com>
2025-09-13 09:39:55 +0300

    refactor: Map creation to smaller functions.

--- src/maps.ts
+++ src/maps.ts
@@ -35,9 +35,14 @@ import {
 export const createMap = (number: number): Array2D<Tile> => {
     const grid = new Array2D<Tile>(10, 50);
 
-    const turningYIndices: number[] = [4, 12, 18, 29, 34, 41, 47];
+    addRoad(grid);
+    addPlants(grid, number);
 
-    const plantPropability = Math.max(0.1, 0.8 - number * 0.05);
+    return grid;
+};
+
+export const addRoad = (grid: Array2D<Tile>): Array2D<Tile> => {
+    const turningYIndices: number[] = [4, 12, 18, 29, 34, 41, 47];
 
     let ixPath = 4;
 
@@ -52,13 +57,23 @@ export const createMap = (number: number): Array2D<Tile> => {
             const ixMin = Math.min(previousIxPath, ixPath);
             const ixMax = Math.max(previousIxPath, ixPath);
 
-            for (let ix = ixMin; ix < ixMax + 1; ix++) {
-                const x = ix * TILE_SIZE;
-
-                const tile = createTile(TileType.Slate, x, y);
-                grid.setValue(ix, iy, tile);
-            }
+            fillHorizontal(grid, iy, ixMin, ixMax, TileType.Slate);
+        } else {
+            const ix = ixPath;
+            const x = ix * TILE_SIZE;
+            const tile = createTile(TileType.Slate, x, y);
+            grid.setValue(ix, iy, tile);
         }
+    }
+
+    return grid;
+};
+
+const addPlants = (grid: Array2D<Tile>, number: number): Array2D<Tile> => {
+    const plantPropability = Math.max(0.1, 0.8 - number * 0.05);
+
+    for (let iy = 0; iy < grid.yCount; iy++) {
+        const y = iy * TILE_DRAW_HEIGHT;
 
         for (let ix = 0; ix < grid.xCount; ix++) {
             const existing = grid.getValue(ix, iy);
@@ -68,17 +83,15 @@ export const createMap = (number: number): Array2D<Tile> => {
 
             const x = ix * TILE_SIZE;
             let tileType =
-                ix === ixPath
-                    ? TileType.Slate
-                    : random() < plantPropability
-                      ? random() < 0.1
-                          ? TileType.Bush
-                          : TileType.Flower
-                      : TileType.Grass;
+                random() < plantPropability
+                    ? random() < 0.1
+                        ? TileType.Bush
+                        : TileType.Flower
+                    : TileType.Grass;
 
             if (isInFrontOfMouseHole(grid, ix, iy)) {
                 // Make sure that the mouse hole is visible.
-                tileType = ix === ixPath ? TileType.Slate : TileType.Grass;
+                tileType = TileType.Grass;
             }
 
             const tile = createTile(tileType, x, y);
@@ -99,6 +112,21 @@ const isInFrontOfMouseHole = (
             ix === Math.floor(grid.xCount / 2)) &&
         (iy === 0 || iy === 1)
     );
+};
+
+const fillHorizontal = (
+    grid: Array2D<Tile>,
+    iy: number,
+    ixMin: number,
+    ixMax: number,
+    type: TileType,
+): void => {
+    for (let ix = ixMin; ix < ixMax + 1; ix++) {
+        const x = ix * TILE_SIZE;
+
+        const tile = createTile(type, x, iy * TILE_DRAW_HEIGHT);
+        grid.setValue(ix, iy, tile);
+    }
 };
 
 export const createIntroMap = (): Array2D<Tile> => {
