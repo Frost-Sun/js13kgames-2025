22b9a5f7545ad83d00420e639994e12f3c51d106
Sami Heikkinen <521design@gmail.com>
2025-10-04 14:28:30 +0300

    chore: update comment

--- src/Level.ts
+++ src/Level.ts
@@ -197,7 +197,7 @@ export class Level implements Area, Space {
                 distance(playerCenter, catCenter) <
                 (this.player.width + this.cat.width) * 0.3
             ) {
-                // Teleport the cat to the player's position so the capture visually completes immediately
+                // Teleport the cat to the mouse's position so the capture visually completes immediately
                 const target = getCenter(this.player);
                 this.cat.x = target.x - this.cat.width / 2;
                 this.cat.y = target.y - this.cat.height * 0.2;
