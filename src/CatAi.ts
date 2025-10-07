f3f54b14e3ac98e72684688367fa41e58df3994a
Tero Jäntti <tkjantti@gmail.com>
2025-10-07 21:37:06 +0300

    fix: Cat speed tuning.

--- src/CatAi.ts
+++ src/CatAi.ts
@@ -301,7 +301,8 @@ export class CatAi {
         this.host.x = INITIAL_CAT_POS.x;
         this.host.y = INITIAL_CAT_POS.y;
         this.host.direction = { x: 0, y: 1 };
-        this.speedMultiplier = this.difficulty === Difficulty.Easy ? 0.6 : 1.0;
+        this.speedMultiplier =
+            this.difficulty === Difficulty.Easy ? 0.65 : 0.95;
     }
 
     getMovement(time: TimeStep): Vector {
