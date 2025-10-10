cb8dd27269f692cc1ad2f505f456c83000b24089
Tero Jäntti <tkjantti@gmail.com>
2025-10-10 21:57:18 +0300

    chore: Tuning gameplay.

--- src/CatAi.ts
+++ src/CatAi.ts
@@ -194,7 +194,7 @@ function reduceObservations(
 
 const getJumpTarget = (mouse: GameObject): Vector => ({
     x: mouse.x + randomMinMax(-0.5, 0.5) * TILE_SIZE,
-    y: mouse.y - 10 * TILE_DRAW_HEIGHT,
+    y: mouse.y - 12 * TILE_DRAW_HEIGHT,
 });
 
 function jumpMovement(
@@ -307,7 +307,7 @@ export class CatAi {
         this.host.x = INITIAL_CAT_POS.x;
         this.host.y = INITIAL_CAT_POS.y;
         this.host.direction = { x: 0, y: 1 };
-        this.speedMultiplier = this.difficulty === Difficulty.Easy ? 0.63 : 0.9;
+        this.speedMultiplier = this.difficulty === Difficulty.Easy ? 0.45 : 0.8;
     }
 
     getMovement(time: TimeStep): Vector {
